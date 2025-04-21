1a657ec24934af9b76bb86e5a60792d441106ca5
Stepan Dementev <dementevssstepan@gmail.com>
2025-04-22 00:15:15 +0300

    Add config

--- back/producer/kafka/kafka_producer.py
+++ back/producer/kafka/kafka_producer.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from confluent_kafka import KafkaAdminClient, NewTopic, Producer
 from aiokafka import AIOKafkaProducer
 import asyncio
+from config import config
 from config.logger_config import logger
 
 class Message:
@@ -10,14 +11,14 @@ class Message:
         self.text = text
         self.tag = tag
 
-config = {
-    'bootstrap.servers': 'localhost:9092',
+producer_config = {
+    'bootstrap.servers': config.Variables.KAFKA_BOOTSTRAP_SERVERS
 }
 
-producer = Producer(config)
+producer = Producer(producer_ config)
 
 def send_message_to_global(message:Message):
-    topic_name='kafka.chat.global'
+    topic_name=config.Variables.KAFKA_GLOBAL_TOPIC
     
     logger.info(f"Sending global message to topic {topic_name}: {message.sender} - {message.text}, tag: {message.tag}")
     producer.produce(topic_name, message.encode('utf-8'))
@@ -25,7 +26,7 @@ def send_message_to_global(message:Message):
     producer.flush()
     
 def send_message_to_chat(chat, message:Message):
-    topic_name = f'kafka.chat.room.{chat}'
+    topic_name = f'{config.Variables.KAFKA_CHAT_TOPIC_PREFIX}.{chat}'
     
     logger.info(f"Sending message to chat {chat} in topic {topic_name}: {message.sender} - {message.text}, tag: {message.tag}")
     producer.produce(topic_name, message.encode('utf-8'))
