27576b7b35cd322cecd119589d421d7e154a14e4
Stepan Dementev <dementevssstepan@gmail.com>
2025-04-21 23:19:37 +0300

    feature: [producer] add logging to kafka producer

--- back/producer/kafka_producer.py
+++ back/producer/kafka_producer.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from confluent_kafka import KafkaAdminClient, NewTopic, Producer
 from aiokafka import AIOKafkaProducer
 import asyncio
+from logger import logger
 
 class Message:
     def __init__(self, sender: str, text: str):
@@ -21,20 +22,27 @@ producer = Producer(config)
 
 def send_tagged_message(message:TaggedMessage):
     topic_name = f'kafka.chat.tagged'
+    
+    logger.info(f"Sending tagged message to topic {topic_name}: {message.sender} - {message.text} - {message.tag}")
     producer.produce(topic_name, message.encode('utf-8'))
+    
     producer.flush()
     
-
 def send_tagged_message(message:Message, tag):
     send_tagged_message(TaggedMessage(message.sender, message.text, tag))
 
 def send_message_to_global(message:Message):
     topic_name='kafka.chat.global'
+    
+    logger.info(f"Sending global message to topic {topic_name}: {message.sender} - {message.text}")
     producer.produce(topic_name, message.encode('utf-8'))
+    
     producer.flush()
     
-
-def send_message_to_chat(chat, message):
+def send_message_to_chat(chat, message:Message):
     topic_name = f'kafka.chat.room.{chat}'
+    
+    logger.info(f"Sending message to chat {chat} in topic {topic_name}: {message.sender} - {message.text}")
     producer.produce(topic_name, message.encode('utf-8'))
+    
     producer.flush()
