362be616342d85771e8bdff47ad6d92e9bb7efe2
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2025-04-28 00:34:19 +0300

    Fix/mongo service (#8)
    * feature: mongodb service
    
    * feature/utility-service-init-service
    
    * feature: [utility-service] init service
    
    * fix: [infra] fix compose
    
    * restore: [producer] restore chanegs
    
    * restore: [producer] restore changes
    
    ---------
    
    Co-authored-by: olesia8novoselova <nov-olesya.m@inbox.ru>
    Co-authored-by: Stepan Dementev <dementevssstepan@gmail.com>

---
+++ back/services/data/users_repository.py
@@ -0,0 +1,38 @@
+from bson import ObjectId  # Import ObjectId to handle MongoDB IDs
+from pydantic import BaseModel
+
+class UsersRepository:    
+    def __init__(self, db):
+        self.collection = db["users"]
+    
+    def find_by_username(self, username: str):
+        user = self.collection.find_one({"username": username})
+        if user:
+            user["_id"] = str(user["_id"])  # Convert ObjectId to string
+        return user if user else None
+
+    def add_chat_subscription(self, username: str, chat: str):
+        user = self.collection.find_one({"username": username})
+        if user:
+            if chat not in user.get("chats", []):
+                self.collection.update_one(
+                    {"username": username},
+                    {"$push": {"chats": chat}}
+                )
+                return True
+        return False
+
+    def save_user_with_username(self, username: str):
+        user = self.collection.find_one({"username": username})
+        if not user:
+            self.collection.insert_one({"username": username, "chats": [], "tag": None})
+            return True
+        return False
+
+class User(BaseModel):
+    username: str
+    chats: list[str]
+    tag: str
+
+    class Config:
+        orm_mode = True
