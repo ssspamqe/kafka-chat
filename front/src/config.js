50e2f786e0f2344a99ce1af57441a1e4098990c4
Stepan Dementev <dementevssstepan@gmail.com>
2025-05-16 22:17:21 +0400

    fix: [infra] fix backend routing :

--- front/src/config.js
+++ front/src/config.js
@@ -1,8 +1,8 @@
-const serviceHost = window.location.hostname === 'localhost' ? 'localhost' : 'mongo-service';
+const serviceHost = window.location.hostname === 'localhost' ? 'localhost' : window.location.hostname;
 export const config = {
     SERVICE_HOST: serviceHost,
-    MONGODB_PORT: 8002,
+    MONGODB_PORT: window.location.hostname === 'localhost' ? 8002 : 80,
     KAFKA_BOOTSTRAP_SERVERS: "localhost:9092",
-    PRODUCER_HOST: 8000,
-    CONSUMER_HOST: 8001
+    PRODUCER_HOST: window.location.hostname === 'localhost' ? 8000 : 80,
+    CONSUMER_HOST: window.location.hostname === 'localhost' ? 8001 : 80
 };
